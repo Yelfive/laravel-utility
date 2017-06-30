962cff294f3c349625274e4f2bb6b4a0d94898f7
Felix <1449704765@qq.com>
2017-06-30 08:50:58 +0800

    modify: to mark UploadedFile as test to pass the is_uploaded_file check for put request

--- Http/SymfonyRequest.php
+++ Http/SymfonyRequest.php
@@ -8,6 +8,7 @@
 namespace fk\utility\Http;
 
 use fk\http\RestfulParser;
+use Illuminate\Http\UploadedFile;
 use Symfony\Component\HttpFoundation\Request as RequestBase;
 
 class SymfonyRequest extends RequestBase
@@ -18,10 +19,16 @@ class SymfonyRequest extends RequestBase
         /** @var array $_post */
         if ($needParse) {
             $_post = $_POST;
-            $_POST = RestfulParser::parseFormData(file_get_contents('php://input'));
+            $_POST = RestfulParser::parseFormData();
+            $_files = $_FILES;
+            // Custom UploadedFile to fool the isValid check
+            $_FILES = array_map(function ($file) {
+                return new UploadedFile($file['tmp_name'], $file['name'], $file['type'], $file['size'], $file['error'], true);
+            }, $_FILES);
         }
         $request = parent::createFromGlobals();
         if ($needParse) $_POST = $_post;
+        if (isset($_files)) $_FILES = $_files;
 
         return $request;
     }
